dc5dea574fa54acfa7715a709a4d1721852b7878
chase <chasebaker21@gmail.com>
2019-12-04 18:06:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function runCode() {
+    
+  let inputText = document.getElementById("input").value;
+
+  if (document.getElementById("fruit").checked == true) {
+    const newLi = document.createElement("li");
+    newLi.innerHTML = inputText;
+    fruitListOutput.append(newLi);
+
+  } else if (document.getElementById("vegetable").checked == true) {
+    const newLi = document.createElement("li");
+    newLi.innerHTML = inputText;
+    vegetableListOutput.append(newLi);
+  }
+}
